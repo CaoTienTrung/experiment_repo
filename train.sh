65d699ce54246c7b0c41266d1aaaa78717856b80
CaoTienTrung <caotientrungproject@gmail.com>
2025-12-11 16:09:13 +0700

    update train_sh

--- train.sh
+++ train.sh
@@ -3,26 +3,29 @@
 set -e
 
 ENV_NAME="myenv"
+MODEL_DIR="vit5-base"
 
-echo "Creating conda environment: $ENV_NAME"
+echo "=== Cloning VietAI/vit5-base model ==="
+git clone https://huggingface.co/VietAI/vit5-base $MODEL_DIR
+
+echo "=== Creating conda environment: $ENV_NAME ==="
 conda create -y -n $ENV_NAME python=3.10
 
-echo "Activating environment: $ENV_NAME"
+echo "=== Activating environment: $ENV_NAME ==="
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate $ENV_NAME
 
-echo "Installing requirements..."
+echo "=== Installing requirements... ==="
 pip install -r requirements.txt
 
-# Train
-echo "Running training..."
+echo "=== Running training... ==="
 python3 run_qa.py \
   --num_train_epochs 300 \
   --learning_rate 1e-5 \
   --train_batch_size 4 \
-  --eval_batch_size 4 \ 
-  --model_name_or_path "VietAI/vit5-base" \
-  --tokenizer_name "VietAI/vit5-base" \
+  --eval_batch_size 4 \
+  --model_name_or_path "$MODEL_DIR" \
+  --tokenizer_name "$MODEL_DIR" \
   --data_dir "Dataset" \
   --checkpoint_dir "checkpoint" \
   --model_pth "model_epoch_2.pth" \
@@ -38,4 +41,4 @@ python3 run_qa.py \
   --eval_interval 5 \
   --do_train "True"
 
-echo "Done!"
+echo "=== Done! ==="
